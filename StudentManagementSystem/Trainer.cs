e382539e668cd93901656694446c13efe81def30
Davidson Leparie <davidsonleparie@gmail.com>
2025-05-15 17:07:33 +0300

    Add Trainer subclass inheriting Person with Department and ShowTrainerDetails

---
+++ StudentManagementSystem/Trainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProjects.StudentManagementSystem
+{
+    internal class Trainer
+    {
+    }
+}
