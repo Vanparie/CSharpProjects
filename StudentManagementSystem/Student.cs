6ad645d62bba46aefe17afd9bb6a9482e8093f9b
Davidson Leparie <davidsonleparie@gmail.com>
2025-05-15 16:03:06 +0300

    Add Student class with StudentId and ShowStudentDetails

---
+++ StudentManagementSystem/Student.cs
@@ -0,0 +1,21 @@
+﻿
+namespace CSharpProjects.StudentManagementSystem
+{
+    public class Student : Person
+    {
+        public string StudentId { get; set; }
+
+        // Student constructor calls the base class constructor using base(...)
+        public Student(string name, string email, string studentId)
+            : base(name, email)
+        {
+            StudentId = studentId;
+        }
+
+        public void ShowStudentDetails()
+        {
+            Console.WriteLine($"🎓 Student: {Name} | Email: {Email} | ID: {StudentId}");
+        }
+    }
+
+}
