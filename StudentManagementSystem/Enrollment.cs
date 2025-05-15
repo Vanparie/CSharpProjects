56113f397494792d0d04c9542a00b26876b57274
Davidson Leparie <davidsonleparie@gmail.com>
2025-05-15 22:18:38 +0300

    Add Enrollment class using descriptive property names and constructor parameters

---
+++ StudentManagementSystem/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CSharpProjects.StudentManagementSystem
+{
+    public class Enrollment
+    {
+        
+        public Student EnrolledStudent { get; set; }
+        public Course SelectedCourse { get; set; }
+
+        // assign student and course during creation
+        public Enrollment(Student enrolledStudent, Course selectedCourse)
+        {
+            EnrolledStudent = enrolledStudent;
+            SelectedCourse = selectedCourse;
+        }
+
+        // Displays enrollment summary
+        public void ShowEnrollment()
+        {
+            Console.WriteLine($"{EnrolledStudent.Name} is enrolled in {SelectedCourse.CourseName}");
+        }
+    }
+}
